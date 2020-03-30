f99965d8a26cc3d2f7d71beacda33d4f3986b5c1
MayronSouza <mayron.souza@gmz.solutions>
2020-03-30 16:55:16 -0300

    Validando as rotas

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express') // Importa o Express
 const cors = require('cors')
+const { errors } = require('celebrate')
 
 const routes = require('./routes')
 
@@ -8,6 +9,7 @@ const app = express() // Instancia o Express
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+app.use(errors())
 
 // Retorna um Server
 app.listen(3334, () => console.log('Server running...'))
